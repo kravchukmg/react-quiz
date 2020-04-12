1ea702ffa0fd281e95dad22b09750333ab40c9f9
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-04-12 12:06:44 +0300

    Added 'Auth' page and created 'input' component.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import classes from './Auth.module.scss'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
+
+    loginHandler = () => {
+        
+    }
+
+    registerHandler = () => {
+
+    }
+
+    sumbitHandler = event => {
+        event.preventDefault()
+    }
+    
     render() {
         return(
-            <>
-                <h1>Auth</h1>
-            </>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизація</h1>
+
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        
+                        <Input 
+                            label="Email"
+                        />
+                        
+                        <Input 
+                            label="Password"
+                            errorMessage={'TEST'}
+                        />
+
+                        <Button 
+                            type="success"
+                            onClick={this.loginHandler}
+                        >
+                            Вхід
+                        </Button>
+                        <Button 
+                            type="primary"
+                            onClick={this.registerHandler}
+                        >
+                            Реєстрація
+                        </Button>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
