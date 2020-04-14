4b014a80e7554657c01a45c7c523609d3d5e42b8
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-04-14 19:51:18 +0300

    Added 'QuizCreator' container and 'select' component.

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,11 +1,121 @@
 import React, { Component } from 'react';
+import classes from './QuizCreator.module.scss'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
+import Select from '../../components/UI/Select/Select'
+import {createControl} from '../../form/formFramework'
+import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
+
+function createOptionControl(number) {
+    return createControl({
+        label: `Варіант ${number }`,
+        errorMessage: 'Значення не може бути пустим',
+        id: number
+    }, {required: true})
+}
+
+function createFormControls() {
+    return {
+        question: createControl({
+            label: 'Введіть питяння',
+            errorMessage: 'Питання не може бути пустим',
+        }, {required: true}),
+        option1: createOptionControl(1),
+        option2: createOptionControl(2),
+        option3: createOptionControl(3),
+        option4: createOptionControl(4)
+    }
+}
 
 export default class QuizCreator extends Component {
+
+    state = {
+        quiz: [],
+        rightAnswerId: 1,
+        formControl: createFormControls()
+    }
+
+    submitHandler = event => {
+        event.preventDefault()
+    }
+
+    addQuestionHandler = () => {
+
+    }
+
+    createQuizHandler = () => {
+
+    }
+
+    changeHandler = (value, controlName) => {
+
+    }
+
+    renderControls() {
+        return Object.keys(this.state.formControl).map((controlName, index) => {
+            const control = this.state.formControl[controlName]
+
+            return (
+                <Auxiliary key={controlName + index}>
+                    <Input 
+                        label={control.label}
+                        value={control.value}
+                        valid={control.valid}
+                        shouldValidate={!!control.shouldValidate}
+                        touched={control.touched}
+                        errorMessage={control.errorMessage}
+                        onChange={event => this.changeHandler(event.target.value, controlName)}
+                    />
+                    { index ===0 ? <hr /> : null }
+                </Auxiliary>
+            )
+        })
+    }
+    
+    selectChangeHandler = event => {
+        this.setState({
+            rightAnswerId: +event.target.value
+        })
+    }
+    
     render() {
+        const select = <Select 
+            label="Вкажіть вірний варіант"
+            value={this.state.rightAnswerId}
+            onChange={this.selectChangeHandler}
+            options={[
+                {text: 1, value: 1},
+                {text: 2, value: 2},
+                {text: 3, value: 3},
+                {text: 4, value: 4}
+            ]}
+        />
+
         return(
-            <>
-                <h1>Quiz Creator</h1>
-            </>
+            <div className={classes.QuizCreator}>
+                <div>
+                    <h1>Створення форми</h1>
+
+                    <form onSubmit={this.submitHandler}>
+                        
+                        { this.renderControls() }
+
+                        { select }
+                        <Button
+                            type="primary"
+                            onClick={this.addQuestionHandler}
+                        >
+                            Добавити запитання
+                        </Button>
+                        <Button
+                            type="succesa"
+                            onClick={this.createQuizHandler}
+                        >
+                            Створити тест
+                        </Button>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
