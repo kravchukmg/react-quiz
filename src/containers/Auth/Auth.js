4b10e5eb3623939d55229509a9a0d6f8d49bdd09
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-04-18 21:39:14 +0300

    Added registration and authorization through Firebase.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.module.scss'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import is from 'is_js'
+import axios from 'axios'
 
 export default class Auth extends Component {
 
@@ -36,12 +37,36 @@ export default class Auth extends Component {
         }
     }
     
-    loginHandler = () => {
-        
+    loginHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        }
+
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC7t4e4Vj02osv9rAKem4KI76nEKmzvoiI', authData)
+
+            console.log(response.data)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
-    registerHandler = () => {
+    registerHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        }
 
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC7t4e4Vj02osv9rAKem4KI76nEKmzvoiI', authData)
+
+            console.log(response.data)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     submitHandler = event => {
